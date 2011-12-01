e2f6a7ac4c79b92cf408b7bbdfea11a2bbc4b317
tbranyen <tim@tabdeveloper.com>
2011-12-01 15:41:50 -0500

    added in pushState snippet to allow better fallback to hash and link hijacking

--- app/index.js
+++ app/index.js
@@ -14,8 +14,33 @@ this.namespace = {
   app: _.extend({}, Backbone.Events)
 };
 
+
+
 jQuery(function($) {
   var app = namespace.app;
+
+  // Only need this for pushState enabled browsers
+  if (Backbone.history && Backbone.history._hasPushState) {
+
+    // Use delegation to avoid initial DOM selection and allow all matching elements to bubble
+    $(document).delegate("a", "click", function(evt) {
+      // Get the anchor href and protcol
+      var href = $(this).attr("href");
+      var protocol = this.protocol + "//";
+
+      // Ensure the protocol is not part of URL, meaning its relative.
+      // Stop the event bubbling to ensure the link will not cause a page refresh.
+      if (href.slice(protocol.length) !== protocol) {
+        evt.preventDefault();
+
+        // Note by using Backbone.history.navigate, router events will not be
+        // triggered.  If this is a problem, change this to navigate on your
+        // router.
+        app.router.navigate(href, true);
+      }
+    });
+
+  }
 
   var Router = Backbone.Router.extend({
     routes: {
@@ -28,5 +53,5 @@ jQuery(function($) {
   });
   
   app.router = new Router();
-  Backbone.history.start();
+  Backbone.history.start({ pushState: true });
 });
