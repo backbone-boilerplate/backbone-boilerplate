466e2afe9e63bdba24c0cf86fd041f81861d3d79
Jaakko Salonen <jaakko.salonen@iki.fi>
2012-03-06 20:42:59 +0200

    Use process.env.PORT instead of hard-coded defaults if specified

--- build/tasks/server/index.js
+++ build/tasks/server/index.js
@@ -14,8 +14,8 @@ task.registerTask("server", "Run development server.", function(prop) {
     favicon: "./favicon.ico",
     index: "./index.html",
 
-    port: 8000,
-    host: "127.0.0.1"
+    port: process.env.PORT || 8000,
+    host: process.env.PORT ? "0.0.0.0" : "127.0.0.1"
   });
 
   options.folders = options.folders || {};
